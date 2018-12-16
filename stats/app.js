8f5966d98bf9d7c144612c3b32031e0938e3c59b
whitepiolin <whitepiolin@gmail.com>
2018-12-16 09:31:30 +0100

    somechanges

--- stats/app.js
+++ stats/app.js
@@ -1,31 +1,28 @@
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
-var mysql = require('mysql');
-var statsTable = require('./routes/statstable.js'); 
-var app = express();
-var connection  = require('express-myconnection'); 
+const express = require("express");
+const routes = require("./routes");
+const http = require("http");
+const mysql = require("mysql");
+const statsTable = require("./routes/statstable.js");
+const app = express();
+const connection = require("express-myconnection");
 
-app.set('port', process.env.PORT || 4300);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(
-    connection(mysql,{
-        host: 'localhost',
-        user: 'root',
-        password : '000000',
-        port : 3306, 
-        database:'realestatemarket'
-},'pool')
+  connection(
+    mysql,
+    {
+      host: "localhost",
+      user: "root",
+      password: "000000",
+      port: 3306,
+      database: "realestatemarket"
+    },
+    "pool"
+  )
 );
-app.get('./views/table.ejs', statsTable.list);
+app.get("./views/table.ejs", statsTable.list);
 app.use(app.router);
-http.createServer(app).listen(app.get(3000), function(){
-    console.log('Express server listening on port ' + app.get(3000));
+http.createServer(app).listen(app.get(3000), function() {
+  console.log("Express server listening on port " + app.get(3000));
 });
